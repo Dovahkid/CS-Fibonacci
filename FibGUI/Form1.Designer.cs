1fe79459fd2ae010f561983b3870875bee0b1272
Dovahkid <35874258+Dovahkid@users.noreply.github.com>
2019-02-14 12:52:13 -0500

    v2.2.3
    removed minimize and maximize buttons and removed ability to resize window

--- FibGUI/Form1.Designer.cs
+++ FibGUI/Form1.Designer.cs
@@ -135,7 +135,10 @@
             this.Controls.Add(this.lblNum);
             this.Controls.Add(this.txtNum);
             this.Controls.Add(this.btnFib);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
             this.Name = "Form1";
             this.Text = "Fibonacci Sequence";
             this.statusStrip1.ResumeLayout(false);
